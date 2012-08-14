95ffd2b3a8e6e8b6166b2a52574e65744c1d98a2
Ira Greenberg <irajgreenberg@gmail.com>
2012-08-14 16:07:04 -0500

    Initial Commit

---
+++ cse1342_example01/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cse1342_example01
+//
+//  Created by Ira on 8/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
